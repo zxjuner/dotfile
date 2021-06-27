4b0e94c231ea0195a1d8da5670c72df10000d8fe
zxjuner <zxjuner@qq.com>
2021-06-27 08:50:09 +0800

    Add iina

--- install-apps.sh
+++ install-apps.sh
@@ -6,20 +6,21 @@ HOMEBREW_NO_AUTO_UPDATE=1
 # Install web apps.
 #
 echo " ----- Install web apps ------"
-brew install java --cask
-brew install alfred --cask
-brew install android-studio --cask
-brew install docker --cask
-brew install sublime-text --cask
-brew install bbedit --cask
-brew install google-chrome --cask
-brew install iterm2 --cask
-brew install sourcetree --cask
-brew install virtualbox --cask
-brew install virtualbox-extension-pack --cask
-brew install imageoptim --cask
-brew install visual-studio-code --cask
-brew install calibre --cask
-brew install xmind --cask
+brew install --cask java
+brew install --cask alfred
+brew install --cask android-studio
+brew install --cask docker
+brew install --cask sublime-text
+brew install --cask bbedit
+brew install --cask google-chrome
+brew install --cask iterm2
+brew install --cask sourcetree
+brew install --cask virtualbox
+brew install --cask virtualbox-extension-pack
+brew install --cask imageoptim
+brew install --cask visual-studio-code
+brew install --cask calibre
+brew install --cask xmind
+brew install --cask iina
 
 echo " ------------ END ------------"
