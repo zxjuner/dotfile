b181fc05401645046e61b8d8286baacdcc964129
Jun <zxjuner@qq.com>
2024-05-21 11:52:21 +0800

    add cask app

--- install-apps.sh
+++ install-apps.sh
@@ -4,23 +4,39 @@
 HOMEBREW_NO_AUTO_UPDATE=1
 
 # Install web apps.
-#
+# virtualbox: 虚拟机
+# alfred: 效率工具
+# sourcetree: git 客户端
+# dbeaver-community: 数据库客户端
+# imageoptim: 图片优化工具
+# calibre: 电子书管理 app
+# xmind: 思维导图
+# iina: 视频播放器
+# kid3: 歌曲信息编辑器
 echo " ----- Install web apps ------"
 brew install --cask java
-brew install --cask alfred
-brew install --cask android-studio
 brew install --cask docker
-brew install --cask sublime-text
-brew install --cask bbedit
-brew install --cask google-chrome
-brew install --cask iterm2
-brew install --cask sourcetree
 brew install --cask virtualbox
 brew install --cask virtualbox-extension-pack
-brew install --cask imageoptim
+
+brew install --cask android-studio
+brew install --cask bbedit
+brew install --cask sublime-text
 brew install --cask visual-studio-code
+
+brew install --cask iterm2
+brew install --cask sourcetree
+brew install --cask dbeaver-community
+
+brew install --cask alfred
+brew install --cask google-chrome
+brew install --cask firefox
+brew install --cask imageoptim
 brew install --cask calibre
 brew install --cask xmind
+
 brew install --cask iina
+brew install --cask kid3
+brew install --cask qbittorrent
 
 echo " ------------ END ------------"
--- install.sh
+++ install.sh
@@ -5,20 +5,25 @@ brew_is_installed() {
 }
 
 echo "安装brew同时会自动安装xcode command line tools:"
-if  [ ! command -v brew >/dev/null 2>&1 ] 
-then
-	echo "没安装,来自动装一个.homebrew!"
+if ! command -v brew &>/dev/null; then
+	echo "没安装,来自动装一个 homebrew!"
 	# 官网推荐的brew安装方式:
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	export SHELL="/bin/zsh"
-
-	# TODO: 如果安装失败,那么退出脚本!
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	if [ $? -eq 0 ]; then
+		echo "homebrew 已安装"
+	else
+		echo "homebrew 安装失败!"
+		exit 1
+	fi
 
 else
 	echo "***"
 	brew update
 	brew upgrade
 fi
+
+# 将环境变量 SHELL 的值设置为 /bin/zsh
+export SHELL="/bin/zsh"
 
 # 启用xcode的命令行开发工具
 echo "Open command line tools first"
